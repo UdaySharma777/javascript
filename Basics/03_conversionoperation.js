979945fffb4488bb46f0f66dde58684bdff4f366
Uday Sharma <uday777sh@gmail.com>
2026-07-11 14:32:24 +0530

    conversion of datatypes

---
+++ Basics/03_conversionoperation.js
@@ -0,0 +1,19 @@
+// let score = "33"
+// let valueInNumber = Number(score)
+// console.log(typeof(score))
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// "33"=>33
+// "33abc"=>Nan
+// true=>1; false=>0
+
+// let isLoggedIn = "1"
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+
+// ""=>false; "deffwdw"=>true
+
+let someNumber=33
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber);
