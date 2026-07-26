fffeb9d5faabdf3ef8c7dc6c3b2695a7ed3f4a59
Uday Sharma <uday777sh@gmail.com>
2026-07-26 14:06:16 +0530

    objects.js

---
+++ 11_Classes_and_OOP/object.js
@@ -0,0 +1,28 @@
+function multipleby5(num){
+    return num*5
+}
+
+multipleby5.power = 2
+
+console.log(multipleby5(5));
+console.log(multipleby5.power);
+console.log(multipleby5.prototype);
+
+function createUser(username,score){
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increment =function(){
+    this.score++
+}
+
+createUser.prototype.printMe = function(){
+    console.log(`Score is ${this.score}`);
+    
+}
+
+const order2 = new createUser("Pizza",200)
+const order1 = new createUser("Sandwhich",100)
+
+order1.printMe()
